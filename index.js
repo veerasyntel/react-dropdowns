6f646c4b646b394712f2149938ad4521f9879420
veerasyntel <brahmi.gonugunta@gmail.com>
2019-10-04 01:06:17 +0530

    3 files modified

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import {Alert} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./style.css";
 
 import Dropdown from "./Dropdown";
 import DropdownMenu from "./DropdownMenu";
-import DatePickerComp from './DatePickerComp';
+import DatePickerComp from "./DatePickerComp";
 
 class App extends Component {
   constructor() {
@@ -66,6 +68,9 @@ class App extends Component {
           handleChange={this.handleChange}
         />
         <DatePickerComp />
+        <Alert variant="dark">
+          This is a warning alert—check it out!
+        </Alert>
       </div>
     );
   }
